e13eb8e0fb42091b288c3cbc49352c2c6c62d4c0
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-07-09 09:36:56 +0300

    feat(apps): update `auth` app

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,11 +1,28 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import logout
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 from apps.accounts.models import User
 
+from .permissions import IsNotAuthenticated
 from .serializers import RegisterAccountSerializer
 
 
 class RegisterAccountAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterAccountSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (IsNotAuthenticated,)
+
+
+@api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
+def logout_user(request):
+    request.user.auth_token.delete()
+
+    logout(request)
+
+    return Response(
+        data={"message": "You have been successfully logged out."},
+        status=status.HTTP_200_OK,
+    )
